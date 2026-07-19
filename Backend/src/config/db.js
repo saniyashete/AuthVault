6ec0c001a4011945c343046a7e289e082be44880
saniyashete <shetesaniya22@gmail.com>
2026-07-19 22:32:04 +0530

    Ready for Render deployment

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -6,6 +6,10 @@ dotenv.config();
 export const connectDB = async (req, res) => {
   try {
     await mongoose.connect(process.env.CONNECTION_STRING);
+    await mongoose.connect(process.env.CONNECTION_STRING);
+
+    console.log("Database Name:", mongoose.connection.name);
+    console.log("Host:", mongoose.connection.host);
     console.log(chalk.greenBright.bold("Connected to mongoDB successfully"));
   } catch (error) {
     console.log(chalk.redBright.bold("Diconnected from MongoDb..."));
