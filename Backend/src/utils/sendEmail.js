0dbbe82edfbae568f4a956043ec7f10fd7d6b521
saniyashete <shetesaniya22@gmail.com>
2026-07-20 10:59:10 +0530

    resend

--- Backend/src/utils/sendEmail.js
+++ Backend/src/utils/sendEmail.js
@@ -3,7 +3,7 @@ import { Resend } from "resend";
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendEmail = async (email, resetURL) => {
-  await resend.emails.send({
+  const { data, error } = await resend.emails.send({
     from: "onboarding@resend.dev",
     to: email,
     subject: "Reset Password",
@@ -11,11 +11,16 @@ const sendEmail = async (email, resetURL) => {
       <h3>Password Reset</h3>
       <p>Click the button below to reset your password.</p>
 
-      <a href="${resetURL}">
-        Reset Password
-      </a>
+      <a href="${resetURL}">Reset Password</a>
     `,
   });
+
+  console.log("Resend Data:", data);
+  console.log("Resend Error:", error);
+
+  if (error) {
+    throw new Error(error.message);
+  }
 };
 
 export default sendEmail;
